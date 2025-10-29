8dba91daaf084e8fbf8cb93c3b96c95f623353fd
underdecanopy-source <underdecanopy@gmail.com>
2025-10-29 08:40:55 +0100

    Update CafeItem.tsx

--- app/(main)/_components/CafeItem.tsx
+++ app/(main)/_components/CafeItem.tsx
@@ -6,7 +6,7 @@ interface CafeItemProps {
   description: string;
 }
 
-export default function CafeItem({ icon, title, description }: CafeItemProps) {
+export function CafeItem({ icon, title, description }: CafeItemProps) {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md text-center flex flex-col">
       <div className="flex items-center justify-center rounded-full bg-orange-100 text-orange-500 mx-auto mb-4 flex-shrink-0" style={{ width: '3rem', height: '3rem' }}>
